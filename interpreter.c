b3d919621752d471408b7cce2afd0a4de75326be
LucasMW <great-lucas@hotmail.com>
2016-03-04 08:01:34 -0300

    fix comment

--- interpreter.c
+++ interpreter.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define DEFAULT_SIZE 100000000 // less than 10 MB
+#define DEFAULT_SIZE 100000000 // less than 100 MB
 #define DEBUG 1
 
 // based on  http://groups.csail.mit.edu/graphics/classes/6.837/F04/cpp_notes/stack1.html
